aa709b379c41071c95cee5ad5fb03502f7e889d2
algomachine007 <amaben202phantom@gmail.com>
2022-10-15 15:26:42 +0100

    adding timeout to baseurl

--- app/api/services/base.ts
+++ app/api/services/base.ts
@@ -7,10 +7,12 @@ export const ENDPOINTS = {
 const { NEXT_APP_RAPID_API_KEY, NEXT_APP_RAPID_HOST } = process.env;
 
 export const api = axios.create({
+  timeout: 1000,
   baseURL: "https://skyscanner50.p.rapidapi.com/api/v1",
   headers: {
     'X-RapidAPI-Key': NEXT_APP_RAPID_API_KEY,
     'X-RapidAPI-Host': NEXT_APP_RAPID_HOST
   }
+
 })
 
