d4f99615244e2fc90a5ab3933c38a9d1f9a8c3d1
algomachine007 <amaben202phantom@gmail.com>
2022-10-15 15:25:20 +0100

    making api calls to flights endpoint

---
+++ app/api/services/base.ts
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const ENDPOINTS = {
+  search: '/searchFlightEverywhereDetails'
+}
+
+const { NEXT_APP_RAPID_API_KEY, NEXT_APP_RAPID_HOST } = process.env;
+
+export const api = axios.create({
+  baseURL: "https://skyscanner50.p.rapidapi.com/api/v1",
+  headers: {
+    'X-RapidAPI-Key': NEXT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Host': NEXT_APP_RAPID_HOST
+  }
+})
+
---
+++ app/api/services/getFlight.ts
@@ -0,0 +1,17 @@
+import { api, ENDPOINTS } from "./base"
+
+export const getFlight = async (origin = 'LOND', countryID = 'US', anytime = true, oneWay = false, currency = 'USD', countryCode = 'US', market = 'en-US') => {
+  return api.get(ENDPOINTS.search,
+    {
+      params: {
+        origin: origin,
+        CountryId: countryID,
+        anytime: anytime,
+        oneWay: oneWay,
+        currency: currency,
+        countryCode: countryCode,
+        market: market
+      }
+    }
+  )
+}
